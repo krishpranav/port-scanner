f298f90eb712246d713b88a53ace56f557999522
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2020-10-12 08:23:04 +0530

    Update portscanner.java

--- portscanner.java
+++ portscanner.java
@@ -1,1 +1,19 @@
+//a simple java tool for scanning open ports on a website
+//tool author: krishpranav
+//tool name: portscanner
 
+import java.net.*;
+class portscanner{
+	public static void main(String arg[]){
+		Socket s;
+
+		for(int i=1;i<65536;i++){
+			try{
+				s=new Socket(arg[0], i);
+				System.out.println("The Port " +i + " Is Open");
+			}catch(Exception e){
+
+			}
+		}
+	}
+}
