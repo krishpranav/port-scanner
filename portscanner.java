4d7691dd207022e0f61db785c877d29c72ee69ff
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2020-10-12 08:21:50 +0530

    Update portscanner.java

--- portscanner.java
+++ portscanner.java
@@ -1,19 +1,1 @@
-// a simple java code for scanning website for open ports
-//author: krisna pranav
-// portscanner
 
-import java.net.*;
-class portscanner{
-	public static void main(String arg[]){
-		Socket s;
-
-		for(int i=1;i<65536;i++){
-			try{
-				s=new Socket(arg[0], i);
-				System.out.println("The Port " +i + " Is Open");
-			}catch(Exception e){
-
-			}
-		}
-	}
-}
